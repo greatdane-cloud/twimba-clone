c6714766f04f0b8a130446f32dca9cc0e17d9f52
greatdane-cloud <vanderbyljeandre@gmail.com>
2024-08-17 16:20:14 +0200

    js chang

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const tweetInput = document.getElementById('tweet-input')
 const tweetBtn = document.getElementById('tweet-btn')
 
 tweetBtn.addEventListener('click', function(){
-    console.log(tweetBtn.value)
+    console.log(tweetInput.value)
 })
 
