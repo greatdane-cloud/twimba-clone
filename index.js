bdd780154bd4cdda5f7b6748624e75926dfa859c
greatdane-cloud <vanderbyljeandre@gmail.com>
2024-08-21 17:09:02 +0200

    js up

--- index.js
+++ index.js
@@ -8,33 +8,42 @@ tweetBtn.addEventListener('click', function(){
 })
 
 function getFeedHtml(){
+    
+    /*
+    Challenge:
+    1. Replace the for of with a forEach.
+    */
+          
+        let feedHtml = ``
+      // for (let tweet of tweetsData){
+        tweetsData.forEach(function(tweet){
 
-    let feedHtml = ``
-
-    for (let tweet of tweetsData){
-        feedHtml += `
+      
+            feedHtml += `
     <div class="tweet">
         <div class="tweet-inner">
-        <img src="${tweet.profilePic}" class="profile-pic">
-        <div>
-            <p class="handle">${tweet.handle}</p>
-            <p class="tweet-text">${tweet.text}</p>
-            <div class="tweet-details">
-                <span class="tweet-detail">
-                    ${tweet.replies.length}
-                </span>
-                <span class="tweet-detail">
-                    ${tweet.likes}
-                </span>
-                <span class="tweet-detail">
-                    ${tweet.retweets}
-                </span>
-            </div>   
-        </div>            
+            <img src="${tweet.profilePic}" class="profile-pic">
+            <div>
+                <p class="handle">${tweet.handle}</p>
+                <p class="tweet-text">${tweet.tweetText}</p>
+                <div class="tweet-details">
+                    <span class="tweet-detail">
+                        ${tweet.replies.length}
+                    </span>
+                    <span class="tweet-detail">
+                        ${tweet.likes}
+                    </span>
+                    <span class="tweet-detail">
+                        ${tweet.retweets}
+                    </span>
+                </div>   
+            </div>            
+        </div>
     </div>
-</div>`
-        
+    `
+       })
+       return feedHtml 
     }
-    return (feedHtml)
-}
-getFeedHtml()
+    
+    console.log(getFeedHtml())
+
